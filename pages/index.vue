ba9db2373ab2e723a4967f3fd762566a370808da
Linh Nguyen <nguyenvanlinh.bk@gmail.com>
2018-04-05 00:00:30 +0700

    Added footer

--- pages/index.vue
+++ pages/index.vue
@@ -13,15 +13,18 @@
         </div>
       </div>
     </section>
+    <app-footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
+import AppFooter from '~/components/AppFooter.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    AppFooter
   }
 }
 </script>
